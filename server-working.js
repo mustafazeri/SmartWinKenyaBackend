c5572fde0405705c285e85cc7f9eaad5e9482101
Joram karumia <joramkarumia205@gmail.com>
2026-09-23 09:23:43 +0300

    Complete SmartWin Kenya backend

--- server-working.js
+++ server-working.js
@@ -4,7 +4,9 @@ const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
